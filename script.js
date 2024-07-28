f02ae93e9fbdea53c4e074766c46103aff8dff41
GiorgiGergedava <giogergedava123@gmail.com>
2024-07-28 18:14:09 +0400

    ADD _ JS

---
+++ script.js
@@ -0,0 +1,40 @@
+// Homework
+//  1
+
+let newNumber = 100;
+if (newNumber < 50) {
+    console.log('less than 50');
+}
+else if (newNumber > 20) {
+    console.log('more than 20');
+}
+else {
+    console.log('error');
+}
+
+
+
+// let resultNew = newNumber < 50 ? 'less than 50' : newNumber > 20 ? 'more than 20' : 'error';
+
+
+
+
+//  2
+let userName = 'Maria'
+
+if (userName) {
+    console.log('Maria' && true);
+}
+else {
+    console.log('error');
+}
+
+
+
+// 3
+
+let Username = 'mariam'
+
+Username || true
+Username = 'mariam' && true
+console.log(true);
